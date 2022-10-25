c2cb6e2c35b458533c83dc8e54f0b99ab7581fcb
Denis Shneider <denis.shneider@minsk.mm.local>
2022-10-25 11:26:16 +0300

    init: start project

--- Readme.md
+++ Readme.md
@@ -1,1 +1,1 @@
-# Hello
+# frontend-lab
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+// libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// components
+import App from 'App';
+// styles
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
